3e1cf5c36a834ee9797e256c0eea19974e11ef02
SerfonteinS <s224032704@mandela.ac.za>
2024-07-26 15:13:01 +0200

    attempt web scraping

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
 # Hello there
 
 print("Testing VS Code.")
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://visiteasterncape.co.za/iconic-eastern-cape/"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+print()
+region_heading = soup.find('h3', class_='elementor-heading-title elementor-size-default')
+print(region_heading.get_text())
+print()
+
+regions_sa = soup.find('div', class_='elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-7579cdf')
+for regionsa in regions_sa:
+    print(regionsa.get_text())
+
+# print(page.text)
+
+
+
