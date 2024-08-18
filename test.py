d10f1ed5f72bd7a6be0289e74142e51561a92b51
SerfonteinS <s224032704@mandela.ac.za>
2024-08-18 15:38:00 +0200

    make some changes

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     print("Testing VS Code.")
 
-    my_file = open("data_extract.txt", "w")
+    my_file = open("init_data_extract.txt", "w")
 
     url = "https://visiteasterncape.co.za/iconic-eastern-cape/"
     page = requests.get(url)
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     my_file.close()
 
-    a_file = open("data_extract.txt", "a")
+    a_file = open("init_data_extract.txt", "a")
 
     regions_sa = soup.find('div', class_='elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-7579cdf')
     for regionsa in regions_sa:
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
     finalfile = open("some_data.txt", "w+")
 
-    with open("data_extract.txt", "r+") as dafile:
+    with open("init_data_extract.txt", "r+") as dafile:
         for line in dafile:
             if not line.isspace():
                 finalfile.write(line) 
